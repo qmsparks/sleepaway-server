6b2a5158a7a73823e75ba5c9079ac831b7b99f78
qsparks <q.m.sparks@gmail.com>
2021-02-12 22:58:12 -0600

    adds scaffolds for session routes and controllers

---
+++ controllers/sessions.js
@@ -0,0 +1,29 @@
+// const db = require('../models');
+
+const create = async (req, res) => {
+  try {
+    
+  } catch (err) {
+    
+  }
+}
+
+const join = async (req, res) => {
+  try {
+    
+  } catch (err) {
+    
+  }
+}
+
+
+// TODO remove user from session
+
+// TODO destroy session
+
+
+module.exports = {
+  create,
+  join,
+
+}
