7b0626c924ca22872773fc12455c4d9b3cb286bd
qsparks <q.m.sparks@gmail.com>
2021-02-12 23:25:01 -0600

    adds user registration functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,9 +4,27 @@ const db = require('../models');
 
 const register = async (req, res) => {
   try {
-    
+    const foundUser = await db.User.findOne({email: req.body.email});
+
+    if (foundUser) return res.send({
+      message: 'Account already registered to email'
+    });
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hash;
+    const createdUser = await db.User.create(req.body);
+
+    return res.status(201).json({
+      status: 201,
+      message: "Success",
+      createdUser
+    });
   } catch (err) {
-    
+    return res.status(500).json({
+      status: 500,
+      message: "Something went wrong. Please try again."
+    })
   }
 }
 
--- routes/auth.js
+++ routes/auth.js
@@ -2,4 +2,6 @@ const router = require('express').Router();
 const ctrl = require('../controllers');
 // const authRequired = require('../middleware/authRequired');
 
+router.post('/register', ctrl.auth.register);
+
 module.exports = router;
